bc6b6f0dd32782e6610a7fbba769161f6a614fc0
nodirdev984 <negmatovn984@gmail.com>
2024-02-22 22:44:13 +0500

    done

---
+++ app/Pricing/page.jsx
@@ -0,0 +1,175 @@
+import React from "react";
+
+const contents = ["./pr1.png", "./pr2.png", "./pr3.png"];
+
+function page() {
+  return (
+    <div className="max-w-[1110px] mx-auto">
+      <img src="./princ.png" className="absolute -z-10 top-0 right-0" alt="" />
+      <img className="mt-[140px] mb-16" src="./Pricing.png" alt="" />
+      <div className="flex gap-10">
+        <div className="flex flex-col max-w-[350px]">
+          <img src={contents[0]} alt="" />
+          <div className="w-full h-[1px] bg-[#36536B] opacity-[25%] mt-6 mb-6"></div>
+          <div className="flex flex-col gap-4">
+            <img width={134} height={28} src="./a1.png" alt="" />
+            <img
+              width={73}
+              height={28}
+              className="w-[73px] h-[28px]"
+              src="./a2.png"
+              alt=""
+            />
+            <img
+              width={134}
+              height={28}
+              className="w-[95px] h-[28px]"
+              src="./a3.png"
+              alt=""
+            />
+            <img
+              width={134}
+              height={28}
+              className="w-[129px] h-[28px]"
+              src="./a4.png"
+              alt=""
+            />
+            <img
+              width={134}
+              height={28}
+              className="w-[88px] h-[28px]"
+              src="./a5.png"
+              alt=""
+            />
+            <img
+              width={134}
+              height={28}
+              className="w-[110px] h-[28px]"
+              src="./a6.png"
+              alt=""
+            />
+            <img
+              width={134}
+              height={28}
+              className="w-[91px] h-[28px]"
+              src="./a7.png"
+              alt=""
+            />
+          </div>
+          <div className="w-full h-[1px] bg-[#36536B] opacity-[25%] mt-6 mb-6"></div>
+          <button className="py-3 px-6 rounded-3xl border border-[#36536B] text-[#36536B] max-w-[200px] active:bg-[#36536B] transition-[800ms] active:text-white  font-bold ">
+            Request Access
+          </button>
+        </div>
+        <div className="flex flex-col max-w-[350px]">
+          <img src={contents[1]} alt="" />
+          <div className="w-full h-[1px] bg-[#36536B] opacity-[25%] mt-6 mb-6"></div>
+          <div className="flex flex-col gap-4">
+            <img width={134} height={28} src="./a1.png" alt="" />
+            <img
+              width={73}
+              height={28}
+              className="w-[73px] h-[28px]"
+              src="./a2.png"
+              alt=""
+            />
+            <img
+              width={134}
+              height={28}
+              className="w-[95px] h-[28px]"
+              src="./a3.png"
+              alt=""
+            />
+            <img
+              width={134}
+              height={28}
+              className="w-[129px] h-[28px]"
+              src="./b2.png"
+              alt=""
+            />
+            <img
+              width={134}
+              height={28}
+              className="w-[88px] h-[28px]"
+              src="./b3.png"
+              alt=""
+            />
+            <img
+              width={134}
+              height={28}
+              className="w-[110px] h-[28px]"
+              src="./a6.png"
+              alt=""
+            />
+            <img
+              width={134}
+              height={28}
+              className="w-[91px] h-[28px]"
+              src="./a7.png"
+              alt=""
+            />
+          </div>
+          <div className="w-full h-[1px] bg-[#36536B] opacity-[25%] mt-6 mb-6"></div>
+          <button className="py-3 px-6 rounded-3xl border border-[#36536B] text-[#36536B] max-w-[200px] active:bg-[#36536B] transition-[800ms] active:text-white  font-bold ">
+            Request Access
+          </button>
+        </div>
+        <div className="flex flex-col max-w-[350px]">
+          <img src={contents[2]} alt="" />
+          <div className="w-full h-[1px] bg-[#36536B] opacity-[25%] mt-6 mb-6"></div>
+          <div className="flex flex-col gap-4">
+            <img width={134} height={28} src="./a1.png" alt="" />
+            <img
+              width={73}
+              height={28}
+              className="w-[73px] h-[28px]"
+              src="./a2.png"
+              alt=""
+            />
+            <img
+              width={134}
+              height={28}
+              className="w-[95px] h-[28px]"
+              src="./a3.png"
+              alt=""
+            />
+            <img
+              width={134}
+              height={28}
+              className="w-[129px] h-[28px]"
+              src="./b2.png"
+              alt=""
+            />
+            <img
+              width={134}
+              height={28}
+              className="w-[88px] h-[28px]"
+              src="./b3.png"
+              alt=""
+            />
+            <img
+              width={134}
+              height={28}
+              className="w-[110px] h-[28px]"
+              src="./c1.png"
+              alt=""
+            />
+            <img
+              width={134}
+              height={28}
+              className="w-[91px] h-[28px]"
+              src="./c2.png"
+              alt=""
+            />
+          </div>
+          <div className="w-full h-[1px] bg-[#36536B] opacity-[25%] mt-6 mb-6"></div>
+          <button className="py-3 px-6 rounded-3xl border border-[#36536B] text-[#36536B] max-w-[200px] active:bg-[#36536B] transition-[800ms] active:text-white  font-bold ">
+            Request Access
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default page;
---
+++ app/page.jsx
@@ -0,0 +1,95 @@
+"use client";
+import React from "react";
+const contents = [
+  {
+    logo: "./logo1.svg",
+    text: "./cont1.png",
+  },
+  {
+    logo: "./logo2.svg",
+    text: "./cont2.png",
+  },
+  {
+    logo: "./logo3.svg",
+    text: "./cont3.png",
+  },
+];
+
+function page() {
+  const hanleShow = () => {
+    const imgEl2 = document.getElementById("img2");
+    const imgEl3 = document.getElementById("img3");
+    imgEl2.classList.remove("hidden");
+    imgEl3.classList.remove("hidden");
+  };
+  return (
+    <div className="max-w-full">
+      <img src="./bg.png" className="absolute -z-10 top-0 right-0" alt="" />
+      <div className=" flex justify-between constainer max-w-[1110px] font-[DM Serif Display] mx-auto items-center ">
+        <div className="max-w-[546px]">
+          <h1 className="text-[72px] leading-[72px]  font-[400] tracking-tighter mb-4 text-[#36536B]">
+            Start building with our APIs for absolutely free.
+          </h1>
+          <div className="max-w-[445px] relative ">
+            <input
+              type="text"
+              placeholder="Enter email address"
+              className="py-3 rounded-xl text-bold placeholder:font-bold placeholder:text-[15px] placeholder:opacity-[50%] text-[#36536B] bg-[#FBFCFE] px-4"
+            />
+            <button className="py-3 px-4 bg-[#BA4270] rounded-3xl active:bg-[#DA6D97] z-10  absolute right-14 text-white">
+              Schedule a Demo
+            </button>
+          </div>
+          <div className="text-[#36536B] mt-2 font-[15px] opacity-[70%]">
+            Have any questions? <span className="font-bold ">Contact Us</span>
+          </div>
+        </div>
+        <img src="./phone.png" className="ml-10" alt="" />
+      </div>
+      <button
+        onClick={() => {
+          hanleShow();
+        }}
+        className="w-full mb-12 relative"
+      >
+        <img id="img" src="./hero.png" className="mx-auto" alt="" />
+        <img
+          id="img2"
+          src="./active1.png"
+          className="z-1 absolute top-[22%] hidden left-[10%]"
+          alt=""
+        />
+        <img
+          id="img3"
+          src="./active3.png"
+          className="z-3 absolute top-[35%] hidden right-[10%]"
+          alt=""
+        />
+      </button>
+      <div className="max-w-[1015px] mx-auto container flex justify-between items-center">
+        <img src="./code.png" alt="" />
+        <img src="./text.png" alt="" />
+      </div>
+      <div className="max-w-full pl-[100px] mb-12 mx-auto flex justify-between items-center">
+        <img src="./text2.png" alt="" />
+        <img src="./bigphone.png" alt="" />
+      </div>
+      <div className="flex max-w-[1110px]  mx-auto justify-between">
+        {contents.map((c) => (
+          <div className="flex max-w-[350px] gap-4 flex-col justify-center">
+            <img
+              width={106}
+              height={106}
+              className="mx-auto"
+              src={c.logo}
+              alt=""
+            />
+            <img src={c.text} alt="" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default page;
